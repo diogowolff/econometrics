c359abfe76a414aa858062a81651bea49095db44
diogowolff <54316784+diogowolff@users.noreply.github.com>
2022-12-02 20:10:30 -0300

    Update PS2.R

--- 2nd part/PS2/PS2.R
+++ 2nd part/PS2/PS2.R
@@ -72,17 +72,78 @@ ggplot(df_itemc, aes(x = qtr, y = mean, color = as.factor(d) )) + geom_line(line
 
 # d)
 
+
+
+# first observation: in order to subtract individual by individual, i need to select only 
+# those in the sample that have both data before and after the intervention
+
+
+# IMPORTANT!!!! THIS MATCHES PEOPLE WHO MIGHT GET CUT WHEN LOOKING AT T=4 OR 5
+# WE MUST CHANGE THE DF GENERATION TO BE DEPENDENT ON TIMEFRAME
+# DOING THAT WE WILL NOT NEED ALL THE OTHER FILTERS I THINK
+
+
+df_itemd = df %>% group_by(id) %>% summarise(max = max(qtr),
+                                                        min = min(qtr)) %>%
+  mutate(bool = ifelse(max*min < 0, 1, 0)) %>%   # this checks if there's a positive and a negative
+  right_join(df) %>%                             # quarter in the data, for every id
+  filter(bool == 1) %>%
+  select(-c(bool, max, min))
+
+
+
 # 2-NN estimator
 
-score_control = df[df$d == 0, ]$p
-score_treatment =  df[df$d == 1, ]$p
+score_control = df_itemd[df_itemd$d == 0, ]     # i generate which is which to get the
+score_treatment =  df_itemd[df_itemd$d == 1, ]  # nearest neighbors using RANN
 
-nn_indexes = nn2(score_control, score_treatment, k = 2)
+nn_indexes = nn2(score_control$p, score_treatment$p, k = 2)
+df_nn = as.data.frame(nn_indexes$nn.idx)
+
 
 nn_did_estimator = function(timeframe) {
-  #mapeia sobre os tratados: pro tratado i, pega a media timeframe p frente - timeframe pra tras
-  # - soma de [i, 1]*media timeframe do controle p frente - ... p tras com [i, 2] do mesmo
+  
+  # given a timeframe, we (for example) take the treatment data that is after treat,
+  # subset it to data that is in the timeframe, then calculate the mean for did
+  
+  yt_t1 = score_treatment[score_treatment$qtr > 0,] %>% filter(abs(qtr) <= timeframe) %>%
+    group_by(id) %>% summarise(mean = mean(earn))
+  yt_t0 = score_treatment[score_treatment$qtr < 0,] %>% filter(abs(qtr) <= timeframe) %>%
+    group_by(id) %>% summarise(mean = mean(earn))
+  
+  yc_t1 = score_control[score_control$qtr > 0,] %>% filter(abs(qtr) <= timeframe) %>%
+    group_by(id) %>% summarise(mean = mean(earn))
+  yc_t0 = score_control[score_control$qtr < 0,] %>% filter(abs(qtr) <= timeframe) %>%
+    group_by(id) %>% summarise(mean = mean(earn))
+  
+  # we then join all this data in the original dataframe to get the dataset with 
+  # differences for every individual
+  # this dataset contains the mean earn for ind. at t1 and t0, whether he's C or T
+  y = df_itemd %>% left_join(yt_t1, by = 'id') %>% 
+    left_join(yt_t0, by = 'id') %>% left_join(yc_t1, by = 'id') %>% 
+    left_join(yc_t0, by = 'id') %>% filter(rowMeans(is.na(.)) < 0.5) %>%
+    rename('yt_t1' = mean.x, 'yt_t0' = mean.y, 'yc_t1' = mean.x.x, 'yc_t0' = mean.y.y) %>%
+    mutate(yt_diff = yt_t1 - yt_t0,
+           yc_diff = yc_t1 - yc_t0)
+  
+  
+  # given the NN we found, we rearrange the control dataset accordingly to the match
+  # in treatment data, then join with all the data and get the yc_diff in the correct order
+  
+  nn1 = score_control[df_nn$V1,] %>% left_join(y) %>% select(yc_diff)
+  nn2 = score_control[df_nn$V2,] %>% left_join(y) %>% select(yc_diff)
+
+  
+  # we now have the vector of yt diffs and the vector of yc diffs all in the right order
+  # so just apply the formula with weight 1/2 for each neighbour and 0 otherwise (just a mean really)
+  
+  alpha = sum(y[y$d == 1, 'yt_diff'] - (nn1+nn2)/2, na.rm = T)/nrow(y)
+  
+  return(alpha)
 }
+
+nn_did_estimator(6)
+
 
 
 # e)
