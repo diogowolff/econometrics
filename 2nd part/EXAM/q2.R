fdb00f1e26330f821b919c47cb8cfa9e37c9147d
diogowolff <54316784+diogowolff@users.noreply.github.com>
2022-12-15 10:42:55 -0300

    Update q2.R

--- 2nd part/EXAM/q2.R
+++ 2nd part/EXAM/q2.R
@@ -56,3 +56,141 @@ results = purrr::map(1:100, ~ optim(c(0.5, 0.5), alpha_gmm_minimizer_500, col_in
 alpha_est_500 = t(matrix(unlist(results), nrow = 2))
 
 colMeans(alpha_est_500)
+var(alpha_est_500[,1])
+var(alpha_est_500[,2])
+
+mean((alpha_est_500[,1]-0.8)^2)
+mean((alpha_est_500[,2]-0.7)^2)
+
+
+
+
+
+
+data_y_mean_1000 = colMeans(Y_mat_1000)
+data_cov_1000 = mapply(function(x, y){cov(x, y)}, as.data.frame(Y_mat_1000), 
+                      as.data.frame(X_mat_1000))
+
+alpha_gmm_minimizer_1000 = function(param, col_index) {
+  data_x = matrix(rep(X_mat_1000[, col_index], 30), nrow = 1000)
+  sim_eps0 = matrix(rgumbel(30*1000), nrow = 1000)
+  sim_eps1 = matrix(rgumbel(30*1000), nrow = 1000)
+  
+  estimated_choice = ifelse(param[1] + param[2]*data_x + sim_eps0 >= sim_eps1, 1, 0)
+  
+  
+  
+  mean_of_sims = colMeans(estimated_choice)
+  
+  mean_of_means = mean(mean_of_sims)
+  
+  
+  
+  
+  cov_of_sims_with_x = mapply(function(x, y){cov(x, y)}, as.data.frame(estimated_choice), 
+                              as.data.frame(data_x))
+  
+  mean_of_covs = mean(cov_of_sims_with_x)
+  
+  
+  moment_diff_vec = c(data_y_mean_1000[col_index] - mean_of_means, 
+                      data_cov_1000[col_index] - mean_of_covs)
+  
+  t(moment_diff_vec) %*% moment_diff_vec
+}
+
+results_1000 = purrr::map(1:100, ~ optim(c(0.5, 0.5), alpha_gmm_minimizer_1000, col_index = .x)$par)
+alpha_est_1000 = t(matrix(unlist(results_1000), nrow = 2))
+
+colMeans(alpha_est_1000)
+var(alpha_est_1000[,1])
+var(alpha_est_1000[,2])
+
+mean((alpha_est_1000[,1]-0.8)^2)
+mean((alpha_est_1000[,2]-0.7)^2)
+
+
+
+
+# d)
+
+
+alpha_gmm_minimizer_500_s100 = function(param, col_index) {
+  data_x = matrix(rep(X_mat_500[, col_index], 100), nrow = 500)
+  sim_eps0 = matrix(rgumbel(100*500), nrow = 500)
+  sim_eps1 = matrix(rgumbel(100*500), nrow = 500)
+  
+  estimated_choice = ifelse(param[1] + param[2]*data_x + sim_eps0 >= sim_eps1, 1, 0)
+  
+  
+  
+  mean_of_sims = colMeans(estimated_choice)
+  
+  mean_of_means = mean(mean_of_sims)
+  
+  
+  
+  
+  cov_of_sims_with_x = mapply(function(x, y){cov(x, y)}, as.data.frame(estimated_choice), 
+                              as.data.frame(data_x))
+  
+  mean_of_covs = mean(cov_of_sims_with_x)
+  
+  
+  moment_diff_vec = c(data_y_mean_500[col_index] - mean_of_means, 
+                      data_cov_500[col_index] - mean_of_covs)
+  
+  t(moment_diff_vec) %*% moment_diff_vec
+}
+
+results_s100 = purrr::map(1:100, ~ optim(c(0.5, 0.5), alpha_gmm_minimizer_500_s100, col_index = .x)$par)
+alpha_est_500_s100 = t(matrix(unlist(results_s100), nrow = 2))
+
+colMeans(alpha_est_500_s100)
+var(alpha_est_500_s100[,1])
+var(alpha_est_500_s100[,2])
+
+mean((alpha_est_500_s100[,1]-0.8)^2)
+mean((alpha_est_500_s100[,2]-0.7)^2)
+
+
+
+
+
+alpha_gmm_minimizer_1000_s100 = function(param, col_index) {
+  data_x = matrix(rep(X_mat_1000[, col_index], 100), nrow = 1000)
+  sim_eps0 = matrix(rgumbel(100*1000), nrow = 1000)
+  sim_eps1 = matrix(rgumbel(100*1000), nrow = 1000)
+  
+  estimated_choice = ifelse(param[1] + param[2]*data_x + sim_eps0 >= sim_eps1, 1, 0)
+  
+  
+  
+  mean_of_sims = colMeans(estimated_choice)
+  
+  mean_of_means = mean(mean_of_sims)
+  
+  
+  
+  
+  cov_of_sims_with_x = mapply(function(x, y){cov(x, y)}, as.data.frame(estimated_choice), 
+                              as.data.frame(data_x))
+  
+  mean_of_covs = mean(cov_of_sims_with_x)
+  
+  
+  moment_diff_vec = c(data_y_mean_1000[col_index] - mean_of_means, 
+                      data_cov_1000[col_index] - mean_of_covs)
+  
+  t(moment_diff_vec) %*% moment_diff_vec
+}
+
+results_1000_s100 = purrr::map(1:100, ~ optim(c(0.5, 0.5), alpha_gmm_minimizer_1000_s100, col_index = .x)$par)
+alpha_est_1000_s100 = t(matrix(unlist(results_1000_s100), nrow = 2))
+
+colMeans(alpha_est_1000_s100)
+var(alpha_est_1000_s100[,1])
+var(alpha_est_1000_s100[,2])
+
+mean((alpha_est_1000_s100[,1]-0.8)^2)
+mean((alpha_est_1000_s100[,2]-0.7)^2)
