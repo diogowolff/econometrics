53d5a4dcec2e3f3224edc4b333e4281cbe640505
Luan Borelli <35273857+luanborelli@users.noreply.github.com>
2022-12-16 20:32:31 -0300

    Q2 data for report

---
+++ 2nd part/EXAM/q2_reported_data.R
@@ -0,0 +1,105 @@
+setwd('C:/Users/Luan Borelli/Documents/GitHub/econometrics/2nd part/EXAM')
+
+load("data_q2.Rdata")
+
+### N = 500, S = 30
+
+# 0,0 
+
+means_30_500_00
+var_alpha0_30_500_00
+var_alpha1_30_500_00
+msqe_alpha0_30_500_00
+msqe_alpha1_30_500_00
+
+# 0.5, 0.5
+
+means_30_500_55
+var_alpha0_30_500_55
+var_alpha1_30_500_55
+msqe_alpha0_30_500_55
+msqe_alpha1_30_500_55
+
+# 0.75, 0.75
+
+means_30_500_7575
+var_alpha0_30_500_7575
+var_alpha1_30_500_7575
+msqe_alpha0_30_500_7575
+msqe_alpha1_30_500_7575
+
+### N = 1000, S = 30 
+
+# 0, 0
+means_30_1000_00
+var_alpha0_30_1000_00
+var_alpha1_30_1000_00
+msqe_alpha0_30_1000_00
+msqe_alpha1_30_1000_00
+
+# 0.5, 0.5 
+means_30_1000_55
+var_alpha0_30_1000_55
+var_alpha1_30_1000_55
+msqe_alpha0_30_1000_55
+msqe_alpha1_30_1000_55
+
+# 0.75, 0.75
+means_30_1000_7575
+var_alpha0_30_1000_7575
+var_alpha1_30_1000_7575
+msqe_alpha0_30_1000_7575
+msqe_alpha1_30_1000_7575
+
+### N = 500, S = 100
+
+# 0,0 
+
+means_100_500_00
+var_alpha0_100_500_00
+var_alpha1_100_500_00
+msqe_alpha0_100_500_00
+msqe_alpha1_100_500_00
+
+# 0.5, 0.5
+
+means_100_500_55
+var_alpha0_100_500_55
+var_alpha1_100_500_55
+msqe_alpha0_100_500_55
+msqe_alpha1_100_500_55
+
+# 0.75, 0.75
+
+means_100_500_75
+var_alpha0_100_500_75
+var_alpha1_100_500_75
+msqe_alpha0_100_500_75
+msqe_alpha1_100_500_75
+
+### N = 1000, S = 100
+
+# 0,0
+
+means_100_1000_00
+var_alpha0_100_1000_00
+var_alpha1_100_1000_00
+msqe_alpha0_100_1000_00
+msqe_alpha1_100_1000_00
+
+# 0.5, 0.5
+
+means_100_1000_55
+var_alpha0_100_1000_55
+var_alpha1_100_1000_55
+msqe_alpha0_100_1000_55
+msqe_alpha1_100_1000_55
+
+# 0.75, 0.75
+
+means_100_1000_7575
+var_alpha0_100_1000_7575
+var_alpha1_100_1000_7575
+msqe_alpha0_100_1000_7575
+msqe_alpha1_100_1000_7575
+
