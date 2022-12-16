a09edf05461eeac426ae069dd5dc3a433a1ef662
Luan Borelli <35273857+luanborelli@users.noreply.github.com>
2022-12-16 12:37:42 -0300

    Comments and plots

--- 2nd part/EXAM/q1.R
+++ 2nd part/EXAM/q1.R
@@ -1,44 +1,126 @@
+############################################
+# Econometrics - 2nd part - Final Exam     #
+# Students: Luan Borelli and Diogo Wolff   #
+############################################
+
 library(tidyverse)
 library(ggplot2)
 library(purrr)
 
 setwd(this.path::here())
 
+######
+# Q1 #
+######
+
 df = read_csv('progresaRDD_exam_q1.csv')
 
-# xnorm already is xi-c
+# Notice that the xnorm variable already is "xi-c".
+
+### (a)
+
+# It is easy to implement these regressions using the poly() function.
+# We are considering here full-specified (complete) polynomials; 
+# i.e., polynomials considering all the degrees.
+
+## Quadratic specification, full polynomial 
+
+quad_reg = lm(y ~ I(xnorm <= 0)*poly(xnorm, 2, raw = TRUE), data = df) # Regressing
+quad_fit = quad_reg$fitted.values # Fitted values 
+quad_cons = quad_reg$coefficients[1] # Constant (for plotting purposes)
+quad_alpha_rdd = quad_reg$coefficients[2] # alpha_RDD estimate.
+
+quad_alpha_rdd
+
+# Plotting (using xnorm in the x-axis): 
+ggplot(data.frame(df$xnorm, quad_fit), aes(x = df.xnorm, y = quad_fit)) + 
+  geom_point(alpha = 0.2) +
+  geom_vline(xintercept = 0, linetype = 'dashed', size=1, color="red") + 
+  geom_hline(yintercept = quad_cons, linetype = 'dashed', size=0.5, color="black", alpha=0.3) +
+  geom_hline(yintercept = quad_cons + quad_alpha_rdd, linetype = 'dashed', size=0.5, color="black", alpha=0.3) +
+  theme_bw() + scale_color_brewer(palette="Paired") + 
+  xlab("Elegibility (xnorm, elegible if <= 0)") +
+  ylab("Scholl enrollment status (Y)") +
+  ggtitle("RDD fitted values, quadratic specification with full polynomial")
+
+# Plotting (using xbin in the x-axis): 
+ggplot(data.frame(df$xbin, quad_fit), aes(x = df.xbin, y = quad_fit)) + 
+  geom_point(alpha = 0.2) +
+  theme_bw() + scale_color_brewer(palette="Paired") + 
+  xlab("Elegibility (bin'd xnorm values)") +
+  ylab("Scholl enrollment status (Y)") +
+  ggtitle("RDD fitted values, quadratic specification with full polynomial (using bin'd xnom values)")
+
+## Cubic specification, full polynomial 
+
+cub_reg = lm(y ~ I(xnorm <= 0)*poly(xnorm, 3, raw = TRUE), data = df) # Regressing
+cub_fit = cub_reg$fitted.values # Fitted values
+cub_cons = cub_reg$coefficients[1] # Constant (for plotting purposes)
+cub_alpha_rdd = cub_reg$coefficients[2] # alpha_RDD estimate.
+
+cub_alpha_rdd
+
+# Plotting (using xnorm in the x-axis): 
+ggplot(data.frame(df$xnorm, cub_fit), aes(x = df.xnorm, y = cub_fit)) + 
+  geom_point(alpha = 0.2) +
+  geom_vline(xintercept = 0, linetype = 'dashed', size=1, color="red") + 
+  geom_hline(yintercept = cub_cons, linetype = 'dashed', size=0.5, color="black", alpha=0.3) +
+  geom_hline(yintercept = cub_cons + cub_alpha_rdd, linetype = 'dashed', size=0.5, color="black", alpha=0.3) +
+  theme_bw() + scale_color_brewer(palette="Paired") + 
+  xlab("Elegibility (xnorm, elegible if <= 0)") +
+  ylab("Scholl enrollment status (Y)") +
+  ggtitle("RDD fitted values, cubic specification with full polynomial")
+
+# Plotting (using xbin in the x-axis): 
+ggplot(data.frame(df$xbin, cub_fit), aes(x = df.xbin, y = cub_fit)) + 
+  geom_point(alpha = 0.2) +
+  theme_bw() + scale_color_brewer(palette="Paired") + 
+  xlab("Elegibility (bin'd xnorm values)") +
+  ylab("Scholl enrollment status (Y)") +
+  ggtitle("RDD fitted values, cubic specification with full polynomial (using bin'd xnom values)")
 
 
-# a)
+## Quartic specification, full polynomial 
 
-# its easy to implement using poly()
+quar_reg = lm(y ~ I(xnorm <= 0)*poly(xnorm, 4, raw = TRUE), data = df) # Regressing
+quar_fit = quar_reg$fitted.values # Fitted values
+quar_cons = quar_reg$coefficients[1] # Constant (for plotting purposes)
+quar_alpha_rdd = quar_reg$coefficients[2] # alpha_RDD estimate.
 
-quad_reg = lm(y ~ I(xnorm <= 0)*poly(xnorm, 2, raw = TRUE), data = df)
-quad_fit = quad_reg$fitted.values
-plot(df$xnorm, quad_fit)
+quar_alpha_rdd
 
-cub_reg = lm(y ~ I(xnorm <= 0)*poly(xnorm, 3, raw = TRUE), data = df)
-cub_fit = cub_reg$fitted.values
-plot(df$xnorm, cub_fit)
+# Plotting (using xnorm in the x-axis): 
+ggplot(data.frame(df$xnorm, quar_fit), aes(x = df.xnorm, y = quar_fit)) + 
+  geom_point(alpha = 0.2) +
+  geom_vline(xintercept = 0, linetype = 'dashed', size=1, color="red") + 
+  geom_hline(yintercept = quar_cons, linetype = 'dashed', size=0.5, color="black", alpha=0.3) +
+  geom_hline(yintercept = quar_cons + quar_alpha_rdd, linetype = 'dashed', size=0.5, color="black", alpha=0.3) +
+  theme_bw() + scale_color_brewer(palette="Paired") + 
+  xlab("Elegibility (xnorm, elegible if <= 0)") +
+  ylab("Scholl enrollment status (Y)") +
+  ggtitle("RDD fitted values, quartic specification with full polynomial")
 
-quar_reg = lm(y ~ I(xnorm <= 0)*poly(xnorm, 4, raw = TRUE), data = df)
-quar_fit = quar_reg$fitted.values
-plot(df$xnorm, quar_fit)
-
-
-# it's working, just have to make some pretty graphs in ggplot
+# Plotting (using xbin in the x-axis): 
+ggplot(data.frame(df$xbin, quar_fit), aes(x = df.xbin, y = quar_fit)) + 
+  geom_point(alpha = 0.2) +
+  theme_bw() + scale_color_brewer(palette="Paired") + 
+  xlab("Elegibility (bin'd xnorm values)") +
+  ylab("Scholl enrollment status (Y)") +
+  ggtitle("RDD fitted values, quartic specification with full polynomial (using bin'd xnom values)")
 
 
 # b)
 
-data_over = df %>% filter(xnorm > 0)
+# Separating data conditional on elegibility.
+data_over = df %>% filter(xnorm > 0) 
 data_under = df %>% filter(xnorm <= 0)
 
+# Defining the triangular kernel function.
 kernel_calculator = function(data, bandwidth) {
   (1-abs(data/bandwidth))*I(abs(data/bandwidth) <= 1)
 }
 
-
+# Defining the theta estimator function.
 theta_estimator = function(x, y, bandwidth) {
   K_vec = kernel_calculator(x, bandwidth)
   
@@ -50,11 +132,25 @@ theta_estimator = function(x, y, bandwidth) {
   solve(lh_mat) %*% rh_vec
 }
 
+# Estimating alpha:
 alp_rdd = (theta_estimator(data_under$xnorm, data_under$y, 15) -
   theta_estimator(data_over$xnorm, data_over$y, 15))[1]
 
-alpha_robustness = map_dbl(10:35, ~ (theta_estimator(data_under$xnorm, data_under$y, .x) -
+alp_rdd
+
+# Computing alpha estimators for bandwiths (h) running from 10 to 35
+alpha_robustness = map_dbl(15:35, ~ (theta_estimator(data_under$xnorm, data_under$y, .x) -
                                    theta_estimator(data_over$xnorm, data_over$y, .x))[1])
+
+alpha_robustness
+
+ggplot(data.frame("bandwith" = 15:35, "alpha_rdd" = alpha_robustness), aes(x = bandwith, y = alpha_rdd)) + 
+  geom_point(size=3) +
+  geom_line(size=1, alpha=0.3) +
+  theme_bw() + scale_color_brewer(palette="Paired") + 
+  xlab("Bandwidth (h)") +
+  ylab("Kernel RDD estimate (alpha_RDD)") +
+  ggtitle("Triangular Kernel RDD estimates as bandwidth (h) varies from 15 to 35")
 
 plot(10:35, alpha_robustness)
 
--- 2nd part/EXAM/q2_padronizada.R
+++ 2nd part/EXAM/q2_padronizada.R
@@ -3,8 +3,25 @@ set.seed(1337)
 library(evd)
 library(furrr)
 
-X_mat_500 = matrix(rlnorm(100*500, log(25/sqrt(25+3)), log(1+3/25)), nrow = 500)
-X_mat_1000 = matrix(rlnorm(100*1000, log(25/sqrt(25+3)), log(1+3/25)), nrow = 1000)
+######
+# Q2 #
+######
+
+### (a)
+
+# Check LaTeX pdf document.
+
+### (b)
+
+# Creating matrices containing 100 vectors of incomes, 
+# with size 500 and 1000 each, respectively, generated from the 
+# log-normal distribution with location 5 and scale sqrt(3).
+X_mat_500 = matrix(rlnorm(100*500, 5, sqrt(3)), nrow = 500) 
+X_mat_1000 = matrix(rlnorm(100*1000, 5, sqrt(3)), nrow = 1000)
+
+# Creating matrices containing 100 vectors of alternative-specific shocks, 
+# with size 500 and 1000 each, respectively, generated from the 
+# gumbel distribution.
 
 eps0_mat_500 = matrix(rgumbel(100*500), nrow = 500)
 eps1_mat_500 = matrix(rgumbel(100*500), nrow = 500)
@@ -12,210 +29,197 @@ eps1_mat_500 = matrix(rgumbel(100*500), nrow = 500)
 eps0_mat_1000 = matrix(rgumbel(100*1000), nrow = 1000)
 eps1_mat_1000 = matrix(rgumbel(100*1000), nrow = 1000)
 
-
+# Generating 500 choice vectors and 1000 choice vectors, respectively,
+# using data generated above. 
 Y_mat_500 = ifelse(0.8 + 0.7*X_mat_500 + eps1_mat_500 >= eps0_mat_500, 1, 0)
 Y_mat_1000 = ifelse(0.8 + 0.7*X_mat_1000 + eps1_mat_1000 >= eps0_mat_1000, 1, 0)
 
-# c)
 
-plan(multisession)
+### (c)
 
+plan(multisession) # For computational performance.
 
-data_y_mean_500 = colMeans(Y_mat_500)
+## SIZE 500 DATASETS ESTIMATES, S = 30:
+
+# Generating the "actual" moments, using the data generated in (b). 
+data_y_mean_500 = colMeans(Y_mat_500) # The "actual" means.
 data_cov_500 = mapply(function(x, y){cov(x, y)}, as.data.frame(Y_mat_500), 
-                      as.data.frame(X_mat_500))
+                      as.data.frame(X_mat_500)) # The "actual" covariances.
 
+# Defining the SMM estimator function for datasets of size 500, with S = 30.
 alpha_gmm_minimizer_30_500 = function(param, col_index) {
   data_x = matrix(rep(X_mat_500[, col_index], 30), nrow = 500)
   sim_eps0 = matrix(rgumbel(30*500), nrow = 500)
   sim_eps1 = matrix(rgumbel(30*500), nrow = 500)
   
   estimated_choice = ifelse(param[1] + param[2]*data_x + sim_eps0 >= sim_eps1, 1, 0)
-  
-  
-  
   mean_of_sims = colMeans(estimated_choice)
-  
   mean_of_means = mean(mean_of_sims)
-  
-  
-  
-  
+
   cov_of_sims_with_x = mapply(function(x, y){cov(x, y)}, as.data.frame(estimated_choice), 
                               as.data.frame(data_x))
-  
+
   mean_of_covs = mean(cov_of_sims_with_x)
-  
-  
   moment_diff_vec = c(data_y_mean_500[col_index] - mean_of_means, 
                       data_cov_500[col_index] - mean_of_covs)
-  
+
   t(moment_diff_vec) %*% moment_diff_vec
 }
 
-results_30_500 = future_map(1:100, ~ optim(c(0.5, 0.5), alpha_gmm_minimizer_30_500, col_index = .x,
+# Optimizing with respect to (alpha_0, alpha_1) for each of the 100 size-500 datasets
+# generated in (a), for S = 30: 
+results_30_500 = future_map(1:100, ~ optim(c(0.5, 0.5), 
+                                    alpha_gmm_minimizer_30_500, col_index = .x,
                                     method = 'BFGS',
                                     control = list('maxit' = '1000')),
-                     .options = furrr_options(seed = T))
-convergence_30_500 = purrr::map(1:100, ~ results_30_500[[.x]]$convergence)
+                                   .options = furrr_options(seed = T))
+
+# Checking convergence status:
+convergence_30_500 = purrr::map(1:100, ~ results_30_500[[.x]]$convergence) 
+
+# Results: 
 alpha_est_30_500 = t(matrix(unlist(purrr::map(1:100, ~ results_30_500[[.x]]$par)), nrow = 2))
 
-colMeans(alpha_est_30_500)
-var(alpha_est_30_500[,1])
-var(alpha_est_30_500[,2])
+colMeans(alpha_est_30_500) # Mean of alpha_0 and alpha_1 estimates, respectively.
+var(alpha_est_30_500[,1]) # Variance of alpha_0 estimates.
+var(alpha_est_30_500[,2]) # Variance of alpha_1 estimates.
 
-mean((alpha_est_30_500[,1]-0.8)^2)
-mean((alpha_est_30_500[,2]-0.7)^2)
+mean((alpha_est_30_500[,1]-0.8)^2) # MSQE of alpha_0 estimates.
+mean((alpha_est_30_500[,2]-0.7)^2) # MSQE of alpha_1 estimates.
 
+## SIZE 1000 DATASETS ESTIMATES, S = 30:
 
-
-
-
-
+# Generating the "actual" moments, using the data generated in (b). 
 data_y_mean_1000 = colMeans(Y_mat_1000)
 data_cov_1000 = mapply(function(x, y){cov(x, y)}, as.data.frame(Y_mat_1000), 
                        as.data.frame(X_mat_1000))
 
+# Defining the SMM estimator function for datasets of size 1000, with S = 30.
 alpha_gmm_minimizer_30_1000 = function(param, col_index) {
+  
   data_x = matrix(rep(X_mat_1000[, col_index], 30), nrow = 1000)
   sim_eps0 = matrix(rgumbel(30*1000), nrow = 1000)
   sim_eps1 = matrix(rgumbel(30*1000), nrow = 1000)
   
   estimated_choice = ifelse(param[1] + param[2]*data_x + sim_eps0 >= sim_eps1, 1, 0)
-  
-  
-  
   mean_of_sims = colMeans(estimated_choice)
-  
   mean_of_means = mean(mean_of_sims)
-  
-  
-  
-  
+
   cov_of_sims_with_x = mapply(function(x, y){cov(x, y)}, as.data.frame(estimated_choice), 
                               as.data.frame(data_x))
   
   mean_of_covs = mean(cov_of_sims_with_x)
-  
-  
   moment_diff_vec = c(data_y_mean_1000[col_index] - mean_of_means, 
                       data_cov_1000[col_index] - mean_of_covs)
-  
   t(moment_diff_vec) %*% moment_diff_vec
 }
 
-results_30_1000 = future_map(1:100, ~ optim(c(0.5, 0.5), alpha_gmm_minimizer_30_1000, col_index = .x,
+# Optimizing with respect to (alpha_0, alpha_1) for each of the 100 size-1000 datasets
+# generated in (b), for S = 30: 
+results_30_1000 = future_map(1:100, ~ optim(c(0.5, 0.5), 
+                                            alpha_gmm_minimizer_30_1000, col_index = .x,
                                             method = 'BFGS',
                                             control = list('maxit' = '1000')),
-                          .options = furrr_options(seed = T))
+                                            .options = furrr_options(seed = T))
+
+# Checking convergence status: 
 convergence_30_1000 = purrr::map(1:100, ~ results_30_1000[[.x]]$convergence)
+
+# Results:
 alpha_est_30_1000 = t(matrix(unlist(purrr::map(1:100, ~ results_30_1000[[.x]]$par)), nrow = 2))
 
-colMeans(alpha_est_30_1000)
-var(alpha_est_30_1000[,1])
-var(alpha_est_30_1000[,2])
+colMeans(alpha_est_30_1000) # Mean of alpha_0 and alpha_1 estimates, respectively.
+var(alpha_est_30_1000[,1]) # Variance of alpha_0 estimates.
+var(alpha_est_30_1000[,2]) # Variance of alpha_1 estimates. 
 
-mean((alpha_est_30_1000[,1]-0.8)^2)
-mean((alpha_est_30_1000[,2]-0.7)^2)
+mean((alpha_est_30_1000[,1]-0.8)^2) # MSQE of alpha_0 estimates.
+mean((alpha_est_30_1000[,2]-0.7)^2) # MSQE of alpha_1 estimates.
 
+### (d)
 
+## SIZE 500 DATASETS ESTIMATES, S = 100:
 
-
-# d)
-
-
-
+# Defining the SMM estimator function for datasets of size 500, with S = 100.
 alpha_gmm_minimizer_100_500 = function(param, col_index) {
   data_x = matrix(rep(X_mat_500[, col_index], 100), nrow = 500)
   sim_eps0 = matrix(rgumbel(100*500), nrow = 500)
   sim_eps1 = matrix(rgumbel(100*500), nrow = 500)
   
   estimated_choice = ifelse(param[1] + param[2]*data_x + sim_eps0 >= sim_eps1, 1, 0)
-  
-  
-  
   mean_of_sims = colMeans(estimated_choice)
-  
   mean_of_means = mean(mean_of_sims)
-  
-  
-  
-  
+
   cov_of_sims_with_x = mapply(function(x, y){cov(x, y)}, as.data.frame(estimated_choice), 
                               as.data.frame(data_x))
-  
+
   mean_of_covs = mean(cov_of_sims_with_x)
-  
-  
   moment_diff_vec = c(data_y_mean_500[col_index] - mean_of_means, 
                       data_cov_500[col_index] - mean_of_covs)
-  
   t(moment_diff_vec) %*% moment_diff_vec
 }
 
-results_100_500 = future_map(1:100, ~ optim(c(0.5, 0.5), alpha_gmm_minimizer_100_500, col_index = .x,
-                                            method = 'BFGS',
-                                            control = list('maxit' = '1000')),
-                          .options = furrr_options(seed = T))
+# Optimizing with respect to (alpha_0, alpha_1) for each of the 100 size-500 datasets
+# generated in (b), for S = 100: 
+results_100_500 = future_map(1:100, ~ optim(c(0.5, 0.5), 
+                                          alpha_gmm_minimizer_100_500, col_index = .x,
+                                          method = 'BFGS',
+                                          control = list('maxit' = '1000')),
+                                          .options = furrr_options(seed = T))
+
+# Checking convergence status:
 convergence_100_500 = purrr::map(1:100, ~ results_100_500[[.x]]$convergence)
+
+# Results: 
 alpha_est_100_500 = t(matrix(unlist(purrr::map(1:100, ~ results_100_500[[.x]]$par)), nrow = 2))
 
-colMeans(alpha_est_100_500)
-var(alpha_est_100_500[,1])
-var(alpha_est_100_500[,2])
+colMeans(alpha_est_100_500) # Mean of alpha_0 and alpha_1 estimates, respectively.
+var(alpha_est_100_500[,1]) # Variance of alpha_0 estimates.
+var(alpha_est_100_500[,2]) # Variance of alpha_1 estimates.
 
-mean((alpha_est_100_500[,1]-0.8)^2)
-mean((alpha_est_100_500[,2]-0.7)^2)
+mean((alpha_est_100_500[,1]-0.8)^2) # MSQE of alpha_0 estimates.
+mean((alpha_est_100_500[,2]-0.7)^2) # MSQE of alpha_1 estimates.
 
+## SIZE 1000 DATASETS ESTIMATES, S = 100:
 
-
-
-
+# Defining the SMM estimator function for datasets of size 1000, with S = 100.
 alpha_gmm_minimizer_100_1000 = function(param, col_index) {
   data_x = matrix(rep(X_mat_1000[, col_index], 100), nrow = 1000)
   sim_eps0 = matrix(rgumbel(100*1000), nrow = 1000)
   sim_eps1 = matrix(rgumbel(100*1000), nrow = 1000)
   
   estimated_choice = ifelse(param[1] + param[2]*data_x + sim_eps0 >= sim_eps1, 1, 0)
-  
-  
-  
   mean_of_sims = colMeans(estimated_choice)
-  
   mean_of_means = mean(mean_of_sims)
-  
-  
-  
-  
+
   cov_of_sims_with_x = mapply(function(x, y){cov(x, y)}, as.data.frame(estimated_choice), 
                               as.data.frame(data_x))
-  
   mean_of_covs = mean(cov_of_sims_with_x)
-  
-  
   moment_diff_vec = c(data_y_mean_1000[col_index] - mean_of_means, 
                       data_cov_1000[col_index] - mean_of_covs)
   
   t(moment_diff_vec) %*% moment_diff_vec
 }
 
-start.time <- Sys.time()
+# Optimizing with respect to (alpha_0, alpha_1) for each of the 100 size-1000 datasets
+# generated in (b), for S = 100:
+results_100_1000 = future_map(1:100, ~ optim(c(0.5, 0.5), 
+                                          alpha_gmm_minimizer_100_1000, col_index = .x,
+                                          method = 'BFGS',
+                                          control = list('maxit' = '1000')), 
+                                          .options = furrr_options(seed = T))
 
-results_100_1000 = future_map(1:100, ~ optim(c(0.5, 0.5), alpha_gmm_minimizer_100_1000, col_index = .x,
-                                             method = 'BFGS',
-                                             control = list('maxit' = '1000')), 
-                              .options = furrr_options(seed = T))
+# Checking convergence status:
 convergence_100_1000 = purrr::map(1:100, ~ results_100_1000[[.x]]$convergence)
 
+# Results: 
 alpha_est_100_1000 = t(matrix(unlist(purrr::map(1:100, ~ results_100_1000[[.x]]$par)), nrow = 2))
 
-colMeans(alpha_est_100_1000)
-var(alpha_est_100_1000[,1])
-var(alpha_est_100_1000[,2])
+colMeans(alpha_est_100_1000) # Mean of alpha_0 and alpha_1 estimates, respectively.
+var(alpha_est_100_1000[,1]) # Variance of alpha_0 estimates.
+var(alpha_est_100_1000[,2]) # Variance of alpha_1 estimates.
 
-mean((alpha_est_100_1000[,1]-0.8)^2)
-mean((alpha_est_100_1000[,2]-0.7)^2)
+mean((alpha_est_100_1000[,1]-0.8)^2) # MSQE of alpha_0 estimates.
+mean((alpha_est_100_1000[,2]-0.7)^2) # MSQE of alpha_1 estimates.
 
-end.time <- Sys.time()
-time.taken <- end.time - start.time
-time.taken
+
+### The End.
